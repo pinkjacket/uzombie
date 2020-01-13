cdc985ceffb03a09636b7536bdbf84344d2685e1
James Donlan <jacketlizard@live.com>
2020-01-13 14:30:19 -0800

    sprites loaded

--- main.lua
+++ main.lua
@@ -1,11 +1,15 @@
 function love.load()
-
+  sprites = {}
+  sprites.player = love.graphics.newImage('sprites/player.png')
+  sprites.bullet = love.graphics.newImage('sprites/bullet.png')
+  sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+  sprites.background = love.graphics.newImage('sprites/background.png')
 end
 
-function love.update()
+function love.update(dt)
 
 end
 
 function love.draw()
-
+ love.graphics.draw(sprites.background, 0, 0)
 end
