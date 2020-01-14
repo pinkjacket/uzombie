403c70e0f6d7a87fcd356b4285327df0ced081cd
James Donlan <jacketlizard@live.com>
2020-01-13 20:35:44 -0800

    zombies spawn randomly when pressing space (for testing)

--- main.lua
+++ main.lua
@@ -9,6 +9,8 @@ function love.load()
   player.x = 200
   player.y = 200
   player.speed = 180
+
+  zombies = {}
 end
 
 function love.update(dt)
@@ -29,8 +31,27 @@ end
 function love.draw()
  love.graphics.draw(sprites.background, 0, 0)
  love.graphics.draw(sprites.player, player.x, player.y, getMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+ for i, z in ipairs(zombies) do
+   love.graphics.draw(sprites.zombie, z.x, z.y)
+ end
 end
 
 function getMouseAngle()
   return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function spawnZombie()
+  zombie = {}
+  zombie.x = math.random(0, love.graphics.getWidth())
+  zombie.y = math.random(0, love.graphics.getHeight())
+  zombie.speed = 100
+
+  table.insert(zombies, zombie)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "space" then
+    spawnZombie()
+  end
 end
